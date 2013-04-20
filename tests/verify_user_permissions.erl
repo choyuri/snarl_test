37cd72d7d876ec00aba5cfba9dcc1bc1db4083f7
Heinz N. Gies <heinz@licenser.net>
2013-04-20 18:14:54 +0200

    Added test for user permissions.

---
+++ tests/verify_user_permissions.erl
@@ -0,0 +1,24 @@
+-module(verify_user_permissions).
+-include_lib("eunit/include/eunit.hrl").
+
+-behavior(riak_test).
+-export([confirm/0]).
+
+-define(USER1, <<"user1">>).
+-define(GROUP1, <<"group">>).
+-define(P1, [<<"some">>, <<"permission">>]).
+-define(P2, [<<"some">>, <<"other">>, <<"permission">>]).
+
+confirm() ->
+    [Node] = rt:deploy_nodes(1),
+    ?assertEqual(ok, rt:wait_until_nodes_ready([Node])),
+    {ok, UUIDU} = rt_snarl:user_add(Node, ?USER1),
+    ?assertEqual(ok, rt_snarl:user_grant(Node, UUIDU, ?P1)),
+    ?assertEqual(true, rt_snarl:allowed(Node, UUIDU, ?P1)),
+
+    {ok, UUIDG} = rt_snarl:group_add(Node, ?GROUP1),
+    ?assertEqual(ok, rt_snarl:group_grant(Node, UUIDG, ?P2)),
+    ?assertEqual(false, rt_snarl:allowed(Node, UUIDU, ?P2)),
+    ?assertEqual(ok, rt_snarl:user_join(Node, UUIDU, UUIDG)),
+    ?assertEqual(true, rt_snarl:allowed(Node, UUIDU, ?P2)),
+    pass.
