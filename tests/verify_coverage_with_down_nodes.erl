c1d6f1505d2f8184f9369aa867e22676a07476e2
Heinz N. Gies <heinz@licenser.net>
2013-10-29 15:02:00 -0700

    Added more tests.

---
+++ tests/verify_coverage_with_down_nodes.erl
@@ -0,0 +1,77 @@
+-module(verify_coverage_with_down_nodes).
+-include_lib("eunit/include/eunit.hrl").
+
+-behavior(riak_test).
+-export([confirm/0]).
+
+-define(USER1, <<"user1">>).
+
+confirm() ->
+    lager:info("Deploying 4 nodes"),
+    %% handoff_concurrency needs to be raised to make the leave operation faster.
+    %% most clusters go up to 10, but this one is one louder, isn't it?
+    [Node1, Node2, Node3, Node4] = Nodes = rt:deploy_nodes(4),
+
+    %% Ensure each node owns 100% of it's own ring
+    lager:info("Ensure each nodes 100% of it's own ring"),
+
+    [rt:wait_until_owners_according_to(Node, [Node]) || Node <- Nodes],
+
+    lager:info("Loading some data up in this cluster."),
+    {ok, UUID1} = rt_snarl:user_add(Node1, ?USER1),
+
+    ?assertEqual({ok, user(?USER1 ,UUID1)},
+                 rt_snarl:user_get(Node1, UUID1)),
+
+    lager:info("joining Node 2 to the cluster... It takes two to make a thing go right"),
+    rt:join(Node2, Node1),
+    wait_and_validate([Node1, Node2], UUID1),
+
+    lager:info("joining Node 3 to the cluster"),
+    rt:join(Node3, Node1),
+    wait_and_validate([Node1, Node2, Node3], UUID1),
+
+    lager:info("joining Node 4 to the cluster"),
+    rt:join(Node4, Node1),
+    wait_and_validate(Nodes, UUID1),
+
+    lager:info("Creating 99 more users."),
+    [ rt_snarl:user_add(Node1, <<"user", Nb/binary>>)
+      || Nb <- [list_to_binary(integer_to_list(N))
+                || N <- lists:seq(2, 100)]],
+    {ok, Users1} = rt_snarl:user_list(Node1),
+    ?assertEqual(100, length(Users1)),
+
+
+    lager:info("Simulate node failure and check if everything is still working."),
+    rt:stop(Node3),
+    ?assertEqual(ok, rt:wait_until_unpingable(Node3)),
+    {ok, Users2} = rt_snarl:user_list(Node1),
+    ?assertEqual(100, length(Users2)),
+
+    pass.
+
+wait_and_validate(Nodes, UUID1) -> wait_and_validate(Nodes, Nodes, UUID1).
+wait_and_validate(RingNodes, UpNodes, UUID1) ->
+    lager:info("Wait until all nodes are ready and there are no pending changes"),
+    ?assertEqual(ok, rt:wait_until_nodes_ready(UpNodes)),
+    ?assertEqual(ok, rt:wait_until_all_members(UpNodes)),
+    ?assertEqual(ok, rt:wait_until_no_pending_changes(UpNodes)),
+    lager:info("Ensure each node owns a portion of the ring"),
+    [rt:wait_until_owners_according_to(Node, RingNodes) || Node <- UpNodes],
+    [rt:wait_for_service(Node, rt:config(rc_services, [riak_kv])) || Node <- UpNodes],
+    lager:info("Verify that you got much data... (this is how we do it)"),
+    [?assertEqual({ok, user(?USER1 ,UUID1)},
+                  rt_snarl:user_get(Node, UUID1)) || Node <- UpNodes],
+    done.
+
+user(Name, UUID) ->
+    [{<<"groups">>,[]},
+     {<<"keys">>,[]},
+     {<<"metadata">>,[]},
+     {<<"name">>,Name},
+     {<<"org">>,<<>>},
+     {<<"orgs">>,[]},
+     {<<"permissions">>,
+      [[<<"users">>, UUID, <<"...">>]]},
+     {<<"uuid">>, UUID}].
---
+++ tests/verify_user_metadata.erl
@@ -0,0 +1,55 @@
+-module(verify_user_metadata).
+-include_lib("eunit/include/eunit.hrl").
+
+-behavior(riak_test).
+-export([confirm/0]).
+
+-define(USER1, <<"user1">>).
+-define(ORG1, <<"org1">>).
+-define(GROUP1, <<"grop1">>).
+
+
+confirm() ->
+    [Node] = rt:deploy_nodes(1),
+    ?assertEqual(ok, rt:wait_until_nodes_ready([Node])),
+
+    %% Set up a user, group and org.
+    {ok, UserID} = rt_snarl:user_add(Node, ?USER1),
+
+    %% Verify we can set metadata
+    ?assertEqual(ok, rt_snarl:user_set(Node, UserID, <<"key">>, 1)),
+    ?assertEqual([{<<"key">>, 1}], metadata(Node, UserID)),
+
+    %% Verify we can change metadata
+    ?assertEqual(ok, rt_snarl:user_set(Node, UserID, <<"key">>, 2)),
+    ?assertEqual([{<<"key">>, 2}], metadata(Node, UserID)),
+
+    %% Verify we can nested metadata
+    ?assertEqual(ok, rt_snarl:user_set(Node, UserID, <<"key2.value">>, 3)),
+    ?assertEqual(ok, rt_snarl:user_set(Node, UserID, [<<"key2">>, <<"value2">>], 4)),
+    ?assertEqual([{<<"key">>, 2},
+                  {<<"key2">>,
+                   [{<<"value">>, 3},
+                    {<<"value2">>, 4}]}],
+                 metadata(Node, UserID)),
+    ?assertEqual(ok, rt_snarl:user_set(Node, UserID, [<<"key2">>, <<"value2">>], 5)),
+    ?assertEqual([{<<"key">>, 2},
+                  {<<"key2">>,
+                   [{<<"value">>, 3},
+                    {<<"value2">>, 5}]}],
+                 metadata(Node, UserID)),
+
+    %% Verify delete metadata
+    ?assertEqual(ok, rt_snarl:user_set(Node, UserID, <<"key">>, delete)),
+    ?assertEqual([{<<"key2">>,
+                   [{<<"value">>, 3},
+                    {<<"value2">>, 5}]}],
+                 metadata(Node, UserID)),
+
+
+    pass.
+
+metadata(Node, UserID) ->
+    {ok, U} = rt_snarl:user_get(Node, UserID),
+    {ok, M} = orddict:find(<<"metadata">>, U),
+    M.
